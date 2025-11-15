b14ae8cee901b3792b03f2f7c247ce60eeb082d5
21Kl3v1s21 <klevis25sfj@gmail.com>
2025-11-15 19:30:14 +0100

    page completed now all thats left is hehehehe

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Arena from "./components/Arena";
 import Characters from "./components/Characters";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 
@@ -10,6 +11,7 @@ function App() {
       <Hero />
       <Characters />
       <Arena />
+      <Footer />
     </div>
   );
 }
